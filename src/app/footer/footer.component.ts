9a22b6642572b017ed4b25d1709e4d6a1fc42b30
Federico Coraglio <federicocoraglio@gmail.com>
2021-02-11 09:23:32 -0300

    Adding dinamic year on footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { NullTemplateVisitor } from '@angular/compiler';
-import { Http } from '@angular/http';
-import { ExchangeService } from '../shared/exchange.service';
+import { Component, OnInit } from "@angular/core";
+import { NullTemplateVisitor } from "@angular/compiler";
+import { Http } from "@angular/http";
+import { ExchangeService } from "../shared/exchange.service";
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent implements OnInit {
-
-  private emailUrl = '/assets/email.php';
+  private emailUrl = "/assets/email.php";
   contactData: any;
   submitted: boolean;
   showMessageSuccess: boolean;
   showMessageError: boolean;
   exchangeData: any = {};
+  year: number;
 
-  constructor(
-    private http: Http) {
-
-  }
+  constructor(private http: Http) {}
 
   ngOnInit() {
     this.showMessageSuccess = false;
@@ -29,8 +26,10 @@ export class FooterComponent implements OnInit {
       name: null,
       email: null,
       phone: null,
-      message: null
-    }
+      message: null,
+    };
+    const today = new Date();
+    this.year = today.getFullYear();
   }
 
   sendMessage() {
@@ -40,43 +39,49 @@ export class FooterComponent implements OnInit {
   }
 
   shouldDisabledButton() {
-    return !this.contactData.name || !this.contactData.phone ||
-      !this.contactData.message || !this.contactData.email;
+    return (
+      !this.contactData.name ||
+      !this.contactData.phone ||
+      !this.contactData.message ||
+      !this.contactData.email
+    );
   }
 
   private sendEmail() {
     this.showMessageSuccess = false;
     this.showMessageError = false;
     this.submitted = true;
-    return this.http.post(this.emailUrl, {
-      name: this.contactData.name,
-      email: this.contactData.email,
-      phone: this.contactData.phone,
-      message: this.contactData.message,
-    }).subscribe(
-      resp => {
-        console.log(resp);
-        console.log('Los datos se enviaron con exito.');
-        this.showMessageSuccess = true;
-        this.showMessageError = false;
-        this.contactData = {
-          name: null,
-          email: null,
-          phone: null,
-          message: null
-        }
-        setTimeout(() => {
+    return this.http
+      .post(this.emailUrl, {
+        name: this.contactData.name,
+        email: this.contactData.email,
+        phone: this.contactData.phone,
+        message: this.contactData.message,
+      })
+      .subscribe(
+        (resp) => {
+          console.log(resp);
+          console.log("Los datos se enviaron con exito.");
+          this.showMessageSuccess = true;
           this.showMessageError = false;
+          this.contactData = {
+            name: null,
+            email: null,
+            phone: null,
+            message: null,
+          };
+          setTimeout(() => {
+            this.showMessageError = false;
+            this.showMessageSuccess = false;
+            this.submitted = false;
+          }, 5000);
+        },
+        (error) => {
+          console.error(error);
           this.showMessageSuccess = false;
+          this.showMessageError = true;
           this.submitted = false;
-        }, 5000);
-      },
-      error => {
-        console.error(error);
-        this.showMessageSuccess = false;
-        this.showMessageError = true;
-        this.submitted = false;
-      }
-    );
+        }
+      );
   }
 }
